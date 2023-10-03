657c259bf0ac7e565c28b6953183f3ff9c674115
Fatima Skandani <fatima.skandrani98@gmail.com>
2023-10-03 11:21:28 +0200

    Route pour accéder au code postal

---
+++ backend/routes/zip.code.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+
+const postalCodesData = require("../test-technique/resources/laposte_hexasmal.json");
+
+router.get("/api/zip-codes/code_postal", (req, res) => {
+  const postalCodeSearch = req.params.codePostal;
+
+  const postalCodeFound = postalCodesData.find(
+    (codePostal) => codePostal.fields.code_postal === postalCodeSearch
+  );
+
+  if (postalCodeFound) {
+    res.json(postalCodeFound);
+  } else {
+    res.status(404).json({ message: "Code postal non trouvé" });
+  }
+});
